991b13e07486c8bc3d3e9b1280e924ce639729d2
Svitlana <potokyvstv@gmail.com>
2026-02-02 09:12:03 +0200

    js hw 12

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -72,46 +72,4 @@ export function hideLoadMoreButton() {
   loadMoreBtn.classList.add('hidden');
 }
 
-// export function createGallery(images) {
-//   const markup = images
-//     .map(
-//       ({
-//         webformatURL,
-//         largeImageURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) => `
-//       <li class="gallery-item">
-//         <a href="${largeImageURL}">
-//           <img src="${webformatURL}" alt="${tags}" />
-//         </a>
-//         <div class="info">
-//           <div class="info-item">
-//             <p class="label">Likes</p>
-//             <p class="value">${likes}</p>
-//           </div>
-//           <div class="info-item">
-//             <p class="label">Views</p>
-//             <p class="value">${views}</p>
-//           </div>
-//           <div class="info-item">
-//             <p class="label">Comments</p>
-//             <p class="value">${comments}</p>
-//           </div>
-//           <div class="info-item">
-//             <p class="label">Downloads</p>
-//             <p class="value">${downloads}</p>
-//           </div>
-//         </div>
-//       </li>
-//     `
-//     )
-//     .join('');
-
-//   gallery.insertAdjacentHTML('beforeend', markup);
-//   lightbox.refresh();
-// }
 
